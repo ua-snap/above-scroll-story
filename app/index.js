4df9f783a74154e2456b928ecbfadbbed900d9e7
Bruce Crevensten <bruce.crevensten@gmail.com>
2018-09-12 16:29:24 -0700

    adds first real content

--- app/index.js
+++ app/index.js
@@ -5,7 +5,15 @@
 // Load application styles
 import 'styles/index.scss'
 
+// Lazy-load images below the fold
+import LazyLoad from 'vanilla-lazyload'
+var lazyLoaded = new LazyLoad({ // eslint-disable-line
+  elements_selector: '.lazy'
+})
+
+// Observations are in the global scope to be referenced by subsections
+import observations from '@/observations' // eslint-disable-line
+
 // App!
 import 'baseMap'
-import 'iceAndSnow'
-import 'iceAndSnowObs'
+import 'iceObservations'
--- scripts/csv2geojson.js
+++ scripts/csv2geojson.js
@@ -2,13 +2,14 @@ const csv = require('csvtojson')
 const GeoJSON = require('geojson')
 const util = require('util')
 const fs = require('fs')
+const _ = require('lodash')
 
-csvFilePath = './snow-ice-obs.csv'
+csvFilePath = './above-observations.csv'
 
 csv({
   noheader: false,
   headers: [
-    'id','harvester','date','permissionGranted','mapVentures','photoIdPrefix','photoFileNames','lat','lng','condition','whatIsPictured','whatisImpact','tripPurpose','frequencyOfObservation','yearFirstObserved','safetyImpact','commonInOtherPlaces','field1','notes','sensitivity'
+    'id','harvester','date','permissionGranted','mapVentures','photoIdPrefix','photoFileNames','lat','lng','condition','whatIsPictured','whatIsImpact','tripPurpose','frequencyOfObservation','yearFirstObserved','safetyImpact','commonInOtherPlaces','field1','notes','sensitivity'
   ]
 })
 .fromFile(csvFilePath)
@@ -18,6 +19,42 @@ csv({
   console.log(output)
 
   fs.writeFile('/tmp/observations.geojson', output, function(err) {
+    if(err) {
+      return console.log(err)
+    }
+  })
+
+  var template = _.template(`
+<div class="observation" data-observation-id="<%= id %>">
+  <div class="observation__date"><%= date %></div>
+  <div class="observation__author"><%= observer %></div>
+  <div class="observation__description">
+    <p><%= pictured %></p>
+    <p><%= impact %></p>
+  </div>
+  <div class="observation__frequency-impact">
+    <p>First seen in <span class="first-observed"><%= first %></span>, <span class="frequency"><%= frequency %></span>, <span class="common-elsewhere"><%= common %></span> and it has a <span class="safety-impact"><%= safety %></span> on travel and safety.</p>
+  </div>
+</div>
+  `)
+  // Also general HTML for use in Narrative
+  var html = ''
+  _.each(jsonObj, (obs) => {
+    html += template({
+      id: obs.id,
+      date: obs.date,
+      observer: obs.harvester,
+      pictured: obs.whatIsPictured,
+      impact: obs.whatIsImpact,
+      first: obs.yearFirstObserved,
+      frequency: obs.frequencyOfObservation,
+      common: obs.commonInOtherPlaces,
+      safety: obs.safetyImpact
+    })
+  })
+
+  console.log(html)
+  fs.writeFile('/tmp/observations.html', html, function(err) {
     if(err) {
       return console.log(err)
     }
