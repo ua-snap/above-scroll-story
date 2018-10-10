c70cabd89088b1d53fbc7d33d7b84760e0e11340
Bruce Crevensten <bruce.crevensten@gmail.com>
2018-10-10 16:47:37 -0700

    content sync

--- app/areaBurnedPlot.js
+++ app/areaBurnedPlot.js
@@ -2,7 +2,7 @@ import Plotly from 'plotly.js-basic-dist'
 
 const years = ['1950', '1960', '1970', '1980', '1990', '2000', '2010', '2020', '2030', '2040', '2050', '2060', '2070', '2080', '2090']
 const historical = [30764, 17505, 10257, 13574, 29616, 63200, 4157]
-const modeled = [ 34226, 10737, 17137, 9029, 36611, 54494, 10088, 23238, 24853, 21833, 21253, 31987, 20525, 30321 ]
+const modeled = [ undefined, undefined, undefined, undefined, undefined, undefined, 10088, 23238, 24853, 21833, 21253, 31987, 20525, 30321 ]
 
 var historicalTrace = {
   x: years,
@@ -22,7 +22,7 @@ var data = [historicalTrace, modelledTrace]
 
 var layout = {
   barmode: 'group',
-  title: 'Total area burned',
+  title: 'Total area burned (km&#x00b2;)',
   font: {
     family: 'Cabin, sans-serif'
   },
